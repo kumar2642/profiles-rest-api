b3461ddb7e3b2dfaa1a0866ec58a98a95f69d32d
Vijay <vijay>
2020-03-08 20:09:13 +0530

    changed deployment shell file to support Ubuntu

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -8,9 +8,8 @@ PROJECT_GIT_URL='https://github.com/kumar2642/profiles-rest-api.git'
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 echo "Installing dependencies..."
-# yum update
-yum install -y virtualenv sqlite supervisor  git
-dnf install python3-pip 
+apt-get update
+apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
