2703e930686877d89123ecd26fb5c02be6101ce2
Vijay <vijay>
2020-03-08 17:26:20 +0530

    Final Commit before deploying

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,5 +1,49 @@
 from rest_framework import serializers
+from profiles_api import models
+
 
 class HelloSerializer(serializers.Serializer):
     """serializes a name field for our API testing"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {
+                    'input_type': 'password'
+                }
+            }
+        }
+
+    def create(self, validated_data):
+
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'], name=validated_data['name'], password=validated_data['password']
+        )
+
+        return user
+
+    def update(self, instance, validated_data):
+        """Handle updating user account"""
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+
+        return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        # read_only_fields = ['user_profile']
+        extra_kwargs = {'user_profile': {'read_only': True}}
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from profiles_api import  serializers
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
+
+from profiles_api import serializers, models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -20,7 +28,7 @@ class HelloApiView(APIView):
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
-    def post(self,resquest):
+    def post(self, resquest):
         """create a hello message with our name"""
         serializer = self.serializer_class(data=resquest.data)
 
@@ -50,7 +58,7 @@ class HelloApiView(APIView):
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
     serializer_class = serializers.HelloSerializer
-    
+
     def list(self, request):
         """Return a hello message."""
 
@@ -95,3 +103,35 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handling creating and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('email', 'name',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnStatus,IsAuthenticated)
+    
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
+
+
+# abe6bd80814cf24918bc1b11db60d5378afbd279 - Akash
